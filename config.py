487f28d7d83cce8f76676d67c4f1fa02017c2d20
Byzxv7 <92144730+Byzxv7@users.noreply.github.com>
2022-01-17 13:39:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://byzxv7:<password>@cluster0.zcqtf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://pzx:aryamto26@cluster0.os7k0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
